2f88fed8a721da28b693b53cbd3cfb966334b54b
BaekRyang <graywolf147@gmail.com>
2023-05-04 11:52:21 +0900

    Asset Added

---
+++ Assets/OfficeAndPoliceStation/Readme.txt
@@ -0,0 +1,18 @@
+Office and Police Station Pack (Modular)
+
+Autosnap:
+Ctrl + L to open snap to grid options. Put autosnap on everytime you want objects to snap immediately to grid.
+Mainly used when building walls and floors and placing doors and some props. Remember to turn off autosnap when you don't use it.
+Recommended to use 0.5 0.5 0.5 values on snap options.
+When using the snap make sure rotation mode is set to Local and transform to Pivot.
+You can also use Unity's ProGrids.
+
+Light Flicker and Optimize can be added to lights and andjusted as desired.
+Light flicker will make light's intensity flicker and optimize will disable light component when at a certain distance from player (game object with player tag).
+
+Wall, ceiling and floor materials can be changed to different ones as they are tilable.
+
+For questions and feedback please send email to support@finwardstudios.com
+
+One or more textures on this 3D-model have been created with images from Textures.com.
+These images may not be redistributed by default. Please visit www.textures.com for more information.
---
+++ Assets/OfficeAndPoliceStation/Scripts/SimplePlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class SimplePlayerController : MonoBehaviour
+{
+    public Camera playerCamera;
+    public float walkSpeed = 1.15f;
+    public float runSpeed = 4.0f;
+    public float lookSpeed = 2.0f;
+    public float lookXLimit = 60.0f;
+    public float gravity = 150.0f;
+
+    CharacterController characterController;
+    Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0;
+    private bool canMove = true;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+
+    void Update()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        if (canMove)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        }
+    }
+}
